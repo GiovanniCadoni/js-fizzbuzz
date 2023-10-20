5657b308ba8330e943cbe76fca523a08d79f08d5
GiovanniCadoni <giocadoni19@gmail.com>
2023-10-20 21:32:43 +0200

    add: javascript and some changes in html file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+//Dichiarazione e inizializzazione di variabili
 const rowElem = document.querySelector(".row");
 console.log(rowElem);
+let grid = "";
+let value = "";
 
+//Ciclo
+for (let i = 1; i <= 100; i++)
+{
+    let result;
+    if (i % 3 === 0 && i % 5 === 0)
+    {
+        result = "fizzbuzz";
+        value = "FizzBuzz"
+    }
+    else if (i % 3 === 0)
+    {
+        result = "fizz";
+        value = "Fizz"
+    }
+    else if (i % 5 === 0)
+    {
+        result = "buzz";
+        value = "Buzz"
+    }
+    else
+    {
+        result = "normal";
+        value = i;
+    }
+    grid += `<div class="square ${result}"> ${value} </div>`;
+    console.log(`iterazione ${i}`, grid);
+}   
+
+//Scrittura nel HTML
+rowElem.innerHTML = grid;
